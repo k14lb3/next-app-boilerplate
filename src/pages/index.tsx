08d1ac608f7c4a95b30340c859170a0e47744719
k14lb3 <k14lb3@gmail.com>
2022-08-08 22:30:00 +0800

    Style main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,12 @@ const Home: NextPage = () => {
         <meta name="description" content="Next.js app boilerplate." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className="flex h-screen flex-col items-center justify-center bg-[#121212] text-center">
+        <h1 className="text-2xl font-bold text-white">
+          Next.js App Boilerplate
+        </h1>
+        <h2 className="mt-2 text-white">(⌐■_■)</h2>
+      </main>
     </>
   );
 };
