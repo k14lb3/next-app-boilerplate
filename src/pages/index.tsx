ca5041bfe9702614634b316dc2f84b2419a4f3a3
k14lb3 <k14lb3@gmail.com>
2022-08-28 12:28:25 +0800

    Change prettier and eslint config for using single quotes on jsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,14 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Next.js App</title>
-        <meta name="description" content="Next.js app boilerplate." />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Next.js app boilerplate.' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
-      <main className="flex h-screen flex-col items-center justify-center bg-[#121212] text-center">
-        <h1 className="text-2xl font-bold text-white">
+      <main className='flex h-screen flex-col items-center justify-center bg-[#121212] text-center'>
+        <h1 className='text-2xl font-bold text-white'>
           Next.js App Boilerplate
         </h1>
-        <h2 className="mt-2 text-white">(⌐■_■)</h2>
+        <h2 className='mt-2 text-white'>(⌐■_■)</h2>
       </main>
     </>
   );
